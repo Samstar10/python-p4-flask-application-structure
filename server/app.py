ace1ac3b60b7d4af18e676ba6982120c0dfe2d2e
Samuel Muli <samuelmuli95@gmail.com>
2024-01-09 11:11:56 +0300

    Completed assignment

--- server/app.py
+++ server/app.py
@@ -1,5 +1,19 @@
 #!/usr/bin/env python3
 
 from flask import Flask
+# from werkzeug.utils import url_quote
 
 app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return '<h1>Welcome to my app!</h1>'
+
+@app.route('/<string:username>')
+def user(username):
+    profile = f'<h1>Profile for {username}</h1>'
+    return profile
+
+
+if __name__ == '__main__':
+    app.run(port=5555)
